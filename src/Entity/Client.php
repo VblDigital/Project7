2d6c02ec496551a5768ff095ea9b720d5269fc67
vbopenclass <vbopenclass@gmail.com>
2020-01-05 17:15:40 +0100

    Setup(Entities)Creation with relation

---
+++ src/Entity/Client.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ */
+class Client
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Product", mappedBy="products")
+     */
+    private $products;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="client")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param mixed $products
+     */
+    public function setProducts($products): void
+    {
+        $this->products = $products;
+    }
+
+    public function addProduct(Product $product)
+    {
+        $this->products[] = $product;
+    }
+
+    /**
+     * @param User $user
+     * @return Client
+     */
+    public function addUser( User $user):self
+    {
+        if ($this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setClient($this);
+        }
+        return $this;
+    }
+    /**
+     * @param User $user
+     * @return Client
+     */
+    public function removeUser ( User $user):self
+    {
+        if($this->users->contains($user)) {
+            $this->users->removeElement($user);
+            if($user->getClient() === $this){
+                $user->setClient(null);
+            }
+        }
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @UniqueEntity(
+ *     fields={"name"},
+ *     message="Ce nom de produit est déjà utilisée"
+ * )
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stock;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Client", inversedBy="products")
+     * @ORM\JoinTable(name="products_clients")
+     */
+    private $client;
+
+    public function __construct()
+    {
+        $this->clients = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?int
+    {
+        return $this->name;
+    }
+
+    public function setName(int $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param $client
+     * @return mixed
+     */
+    public function setClient ( $client )
+    {
+        $this->client = $client;
+        return $client;
+    }
+
+    public function addClient(Client $client)
+    {
+        $client->addProduct($this);
+        $this->client[] = $client;
+    }
+}
