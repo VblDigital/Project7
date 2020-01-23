b4a3905026f86fefe18861612e1fe6db00e26c9d
vbopenclass <vbopenclass@gmail.com>
2020-01-23 11:23:19 +0100

    Merge with Athentication

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,6 @@ use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
